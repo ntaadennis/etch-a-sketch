128a2669c657f29fe4d87a352a15c05a59713a8a
xvma <ntaadennis@gmail.com>
2022-10-23 22:37:47 +0200

    create grid

---
+++ script.js
@@ -0,0 +1,8 @@
+const container = document.querySelector('.container');
+
+for (let i = 0; i < 256; i++) {
+    let square = document.createElement('div');
+    square.classList.add('square');
+    container.append(square);
+
+}
